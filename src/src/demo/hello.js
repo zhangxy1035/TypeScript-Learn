28954531923ed995f5e4dc8eab3a7d4caa9c18fe
cancer-PC\cancer <zhangxy1035@gmail.com>
2016-09-24 23:01:58 +0800

    det

--- src/src/demo/hello.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by cancer on 2016-09-21.
- */
-alert('hello world in TypeScript!');
-function area(shape, width, height) {
-    var area = width * height;
-    return "I'm a " + shape + " with an area of " + area + " cm squared.";
-}
-document.body.innerHTML = area("rectangle", 30, 15);
--- src/src/demo/interface.js
+++
@@ -1,8 +0,0 @@
-function area(shape) {
-    var area = shape.width * shape.height;
-    return "I'm " + shape.name + " with area " + area + " cm squared";
-}
-//传入参数时
-console.log(area({ name: "rectangle", width: 30, height: 15 }));
-console.log(area({ name: "square", width: 30, height: 30, color: "blue" }));
-//# sourceMappingURL=interface.js.map
