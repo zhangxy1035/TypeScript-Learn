33fdf81d0e204586b8bfa4b6640714554d253419
cancer-PC\cancer <1035210017@qq.com>
2016-09-25 21:45:01 +0800

    d3

---
+++ src/src/demo/interface.js
@@ -0,0 +1,6 @@
+function area(shape) {
+    var area = shape.width * shape.height;
+    return "I'm " + shape.name + " with area " + area + " cm squared";
+}
+console.log(area({ name: "rectangle", width: 30, height: 15 }));
+console.log(area({ name: "square", width: 30, height: 30, color: "blue" }));
