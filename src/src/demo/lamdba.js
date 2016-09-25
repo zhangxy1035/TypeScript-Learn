6dbf1e71a1d2f1ad890a280066f8bd5a3f69fbc3
cancer-PC\cancer <1035210017@qq.com>
2016-09-25 22:01:07 +0800

    d4

---
+++ src/src/demo/lamdba.js
@@ -0,0 +1,15 @@
+/**
+ * Created by cancer on 2016-09-25.
+ */
+var shape = {
+    name: "rectangle",
+    popup: function () {
+        var _this = this;
+        console.log('This inside popup(): ' + this.name);
+        setTimeout(function () {
+            console.log('This inside setTimeout(): ' + _this.name);
+            console.log("I'm a " + _this.name + "!");
+        }, 3000);
+    }
+};
+shape.popup();
---
+++ src/src/demo/lamdba.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by cancer on 2016-09-25.
+ */
+var shape = {
+    name: "rectangle",
+    popup: function() {
+
+        console.log('This inside popup(): ' + this.name);
+
+        setTimeout( () => {
+            console.log('This inside setTimeout(): ' + this.name);
+            console.log("I'm a " + this.name + "!");
+        }, 3000);
+
+    }
+};
+
+shape.popup();
