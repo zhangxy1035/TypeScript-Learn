147627b2e963c96ffae1e59182723161a57f0dfc
cancer-PC\cancer <1035210017@qq.com>
2016-09-25 21:16:01 +0800

    d1

--- src/src/demo/hello.ts
+++ src/src/demo/hello.ts
@@ -2,9 +2,3 @@
  * Created by cancer on 2016-09-21.
  */
 alert('hello world in TypeScript!');
-function area(shape: string, width: number, height: number) {
-    var area = width * height;
-    return "I'm a " + shape + " with an area of " + area + " cm squared.";
-}
-
-document.body.innerHTML = area("rectangle", 30, 15);
